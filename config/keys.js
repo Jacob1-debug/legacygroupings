0b33e77f7d0805b8d17b1a3b3ff7e6415d7743c6
JACOB K ROTICH <70254498+Jacob1-debug@users.noreply.github.com>
2023-03-03 18:54:30 -0800

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '625256523481-bjaenea4s7trmles0ui690a2egatn81t.apps.googleusercontent.com',
+        clientSecret: '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'
+    },
+    mongodb: {
+        dbURI: 'mongosh "mongodb+srv://cluster0.s5tupaf.mongodb.net/myFirstDatabase" --apiVersion 1 --username jacobrotich'
+    },
+    session: {
+        cookieKey: 'sJ8aM3nqR5fPcZ'
+    }
+};
